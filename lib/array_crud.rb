d42055c4f6e0c9aea2663fb52e6413b2828bcdd3
Mike McNeeley <mmcneeley@gmail.com>
2018-02-28 17:00:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Mike"]
+  ["Mike", 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
